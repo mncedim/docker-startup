a4e5662665205bc5046727294870f34868fd8e3c
mncedim <mncedim@gmail.com>
2017-04-20 20:02:18 +0200

    add more xdebug notes for phpstorm

--- index.php
+++ index.php
@@ -6,7 +6,4 @@
  * Time: 6:01 PM
  */
 
-$result = mail('admin@startup.com', 'Project Startup', 'Email successfully sent via mailhog');
-var_dump($result ? 'email function working':'email function test failed');
-
 phpinfo();
